1eeb50486693ec1a57bf29ce0e465b6b5ca86e5e
James McCabe <jpmccabe@mit.edu>
2014-01-26 02:11:44 -0500

    fixed outline of camera processor 4.

--- Maslab Robot/src/camera/CameraProcessor4.java
+++ Maslab Robot/src/camera/CameraProcessor4.java
@@ -10,7 +10,7 @@ import org.opencv.imgproc.Imgproc;
  * Image processor for reactors.
  */
 public class CameraProcessor4 {
-    private double wallCloseness;
+    private double reactorDistance;
     private Mat processedImage;
 
     static {
@@ -19,40 +19,13 @@ public class CameraProcessor4 {
 
 
     public CameraProcessor4(){
-        wallCloseness = Double.MAX_VALUE;
+        reactorDistance = Double.MAX_VALUE;
         processedImage  = null;
     }
 
 
     public void processImage(Mat imageToProcess) {
-        Mat processedImage = imageToProcess.clone();
-
-        //Blue Walls
-        Core.inRange(processedImage, new Scalar(90, 75,10), new Scalar(120, 255, 255), processedImage);
-
-        Imgproc.Canny(processedImage, processedImage, 15, 200);     
-        double[] blueStripe= new double[64];
-        for(int x=5;x<640;x+=10){
-            int firstPixel=0;
-            int secondPixel=0;
-            for(int y=240;y>0;y--){
-                if( processedImage.get(y,x)[0]==255){
-                    if (firstPixel==0) firstPixel=y;
-                    else secondPixel=y;
-                }
-                if (secondPixel!= 0) break;
-            }
-            blueStripe[x/10]=firstPixel-secondPixel;
-            Core.line(processedImage, new Point(x,firstPixel), new Point(x,secondPixel), new Scalar(255,0,0));
-        }
-
-
-        Imgproc.cvtColor(processedImage,processedImage,Imgproc.COLOR_GRAY2RGB);
         
-        synchronized(this){
-            wallCloseness = 600/blueStripe[2];
-            this.processedImage = processedImage;
-        }
     }
     
     
@@ -60,7 +33,7 @@ public class CameraProcessor4 {
      * @return how close a reactor was in the last image
      */
     synchronized public double getDistanceToReactor(){
-        return wallCloseness;
+        return reactorDistance;
     }
     
     
